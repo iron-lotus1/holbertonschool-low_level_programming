54b8a43745d963ba4ad571b077912d8b8e338768
Kev <kevin.hulbert93@gmail.com>
2026-07-06 22:20:01 +1000

    updated betty restrictions

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /*
  * main - entry point
  *
@@ -10,21 +9,16 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
 	}
-	else if (n == 0)
-	{
+	else if (n == 0){
 		printf("%d is zero\n", n);
-
 	}
-	else
-	{
+	else{
 		printf("%d is negative\n", n);
 	}
 	return (0);
