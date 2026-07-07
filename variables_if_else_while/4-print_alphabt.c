da865c8ffaacc6e605ef33c9d4da421feafe7f51
Kev <kevin.hulbert93@gmail.com>
2026-07-07 22:05:16 +1000

    fixed betty checker compliance for 4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,13 +6,12 @@
 */
 int main(void)
 {
-
 	char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	if (alphabet != 'q' && alphabet != 'e')
-	{
+if (alphabet != 'q' && alphabet != 'e')
+{
 putchar(alphabet);
-	}
+}
 putchar('\n');
-return(0);
+return (0);
 }
