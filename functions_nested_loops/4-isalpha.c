0a1c8f5154329f96f3748d80b9e03ae9e78d2133
Kev <kevin.hulbert93@gmail.com>
2026-07-08 22:19:34 +1000

    add 4 isalpha to directory

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - checks for alphabetic char
+ * @c: the char to be checked
+ *
+ * Return: 1 if c is a letter otherwis 0
+ */
+int _isalpha(int c);
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+return (0);
+}
