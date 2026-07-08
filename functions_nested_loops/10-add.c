eff2499e9a851e59f0dc0190211fea28167f4e51
Kev <kevin.hulbert93@gmail.com>
2026-07-08 23:09:01 +1000

    add 10 add to DIR

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds 2 int
+ * @int: in the var
+ * @num1 - first int
+ * @num2 - second int
+ * Return: results
+ */
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
