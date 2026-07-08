158e09450593c892cbdf25666e8f84f383f6e2a7
Kev <kevin.hulbert93@gmail.com>
2026-07-08 22:21:02 +1000

    add 6 abs to directory

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Computes absolute val of int
+ * @n: int to be checked
+ * Return: val of int
+ */
+int _abs(int);
+{
+if (n < 0)
+{
+return (-n);
+}
+return (n);
+}
