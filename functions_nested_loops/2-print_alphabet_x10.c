e9a95a24f600b6d6aaff6102a48af1beb48c3170
Kev <kevin.hulbert93@gmail.com>
2026-07-08 17:36:42 +1000

    add 2-print x10 to directory

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
 void print_alphabet_x10(void)
 {
 int i;
-char let;
-for (i = 0; i < 10; i++)
+int let;
+for (i = 0; i < 9; i++)
 {
 for (let = 'a'; let <= 'z'; let++)
 {
 _putchar(let);
 }
-_putchar('n');
+_putchar('\n');
 }
 }
