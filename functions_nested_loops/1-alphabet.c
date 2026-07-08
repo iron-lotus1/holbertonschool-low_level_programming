0587c9c8232ae13728514a607f7ef15b60342ce1
Kev <kevin.hulbert93@gmail.com>
2026-07-08 17:08:37 +1000

    add 1-alphabet.c to function nested loops directory

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - Prints the alphabet in lowercase.
+*
+* Return: void
+*/
+void print_alphabet(void)
+{
+char l;
+for (l = 'a'; l <= 'z'; l++)
+{
+_putchar(l);
+}
+_putchar('\n');
+}
