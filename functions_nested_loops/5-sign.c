0caf6b96acf5f6d283cc65fe97cc9cf275727e64
Kev <kevin.hulbert93@gmail.com>
2026-07-08 22:20:31 +1000

    add 5 sign to directory

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1 or else 0 otherwise -1
+ */
+int print_sign(int n);
+{
+if (n > 0)
+{
+write(1, "+", 1);
+return (1);
+}
+else if (n == 0)
+{
+write(1, "0", 1);
+return (0);
+}
+else
+{
+write(1, "-", 1);
+return (-1);
+}
+}
