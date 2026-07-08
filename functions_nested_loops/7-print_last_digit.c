f90cf4422b0234a1dfcb04048c40c63cedd35e62
Kev <kevin.hulbert93@gmail.com>
2026-07-08 22:22:01 +1000

    add 7-print to functions nested loops directory

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_last_digit - Prints the last dig of a num
+ * @n: char to be processed
+ * Return: value on last num
+ */
+int print_last_digit(int n);
+{
+int last_digit = n % 10;
+if (last_digit < 0)
+last_digit *= -1;
+_putchar(last_digit + "0");
+return (last_digit)
+}
