5a1c5a0af863271e216ae771cb303765bd5b6328
Kev <kevin.hulbert93@gmail.com>
2026-07-09 14:59:19 +1000

    add to DIR

--- intro_debugging/row_sums.c
+++ intro_debugging/row_sums.c
@@ -14,16 +14,14 @@ int row_sum(int row, int cols)
 int total_sum(int rows, int cols)
 {
     int r;
-    int total = 0;
     int current = 0;
 
     for (r = 1; r <= rows; r++)
     {
         current += row_sum(r, cols);
-        total += current;
     }
 
-    return total;
+    return current;
 }
 
 /* -- DO NOT Modify the code below this line -- */
