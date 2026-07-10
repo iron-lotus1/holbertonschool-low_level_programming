2901bece14bb47b28095ff51bf8aeffb634a23d4
Kev <kevin.hulbert93@gmail.com>
2026-07-10 15:37:29 +1000

    add code to 3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,14 @@
-#include
+#include "main.h"
+#include <stdio.h>
 /**
-* main - Entry point
-*
+* print_numbers - prints numbers from 0 to 9
+* num: the number used
 * Return: Always (0) (Success)
 */
+void print_numbers(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+_putchar((num % 10) + '0');
+_putchar('\n');
+}
