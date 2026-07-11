3e9e37c5cf48a5facdf27cad38045ab9f48bc390
Kev <kevin.hulbert93@gmail.com>
2026-07-11 17:44:23 +1000

    add code to 5-more_numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,13 +1,24 @@
 #include "main.h"
 /**
-* main - Entry point
+* more_numbers - prints 0 to 14; 10 times
 *
-* Return: Always (0) (Success)
+* Return:
 */
 void more_numbers(void)
 {
+int i;
+int num;
 
-    _putchar();
-    _putchar();
-    _putchar('\n');
+for (i = 0; i < 10; i++)
+{
+for (num = 0; num <= 14; num++)
+{
+if (num > 9)
+{
+_putchar((num / 10) + '0');
+}
+_putchar((num % 10) + '0');
+}
+_putchar('\n');
+}
 }
