aa7d4cc3756e91bff65ff90c75bdade203d6f059
Kev <kevin.hulbert93@gmail.com>
2026-07-11 21:47:35 +1000

    add 6-print lline to more functions nested loops DIR

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,19 @@
 #include "main.h"
+#include <unistd.h>
 /**
 * print_line - function that draws a strait line in terminal
-*
+* @n: The num of times the character _ should be printed
 * Return: Always (0) (Success)
 */
 void print_line(int n)
 {
-int or char 
-//n = "_" ascii 95
-//n <= 0 print \n
-
-
-    _putchar('\n');
+int i;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
 }
