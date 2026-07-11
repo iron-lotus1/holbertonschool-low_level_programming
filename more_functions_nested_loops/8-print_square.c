07f03247e6dee8b9da1f7acca4a2ea043be91c23
Kev <kevin.hulbert93@gmail.com>
2026-07-11 21:48:57 +1000

    add 8-print squares to more functions nested loops DIR

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,12 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* print_square - Entry point
-*
-* Return: 
+* print_square - Prints a square of a given size
+* @size: the width and Height
+* Return:
 */
 void print_square(int size)
 {
-//size = to the size of the square
-//size is <= 0 ; function should print newline
-    _putchar('\n');
+int row;
+int col;
+{
+_putchar('\n');
+return;
+}
+for (col = 0; row < size; row++)
+{
+for (col = 0; col < size; col++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
