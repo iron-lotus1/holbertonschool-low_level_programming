37a38a1e03f8d62a05c8966882470b46d49a2f02
Kev <kevin.hulbert93@gmail.com>
2026-07-17 10:19:05 +1000

    add 0-reset to 98 to the pointers arrays string DIR

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * reset_to_98 - Updates the value of an integer pointer to 98.
+ * @n: A pointer to the integer to be updated.
+ *
+ * Return: Always 0 (or void if the function returns nothing).
+ */
+int reset_to_98(int *n)
+{
+*n = 98;
+}
