0046d331309127b8c9838d8aad1880bdb9bb2c1c
Kev <kevin.hulbert93@gmail.com>
2026-07-25 17:31:07 +1000

    add 0-malloc_checked.c to DIR

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+#include "main.h"
 /**
  * malloc_checked - allocates memory and exits on failure
  * @b: number of bytes to allocate
@@ -9,9 +9,7 @@
 void *malloc_checked(unsigned int b)
 {
     void *ptr = malloc(b);
-
     if (ptr == NULL)
-        exit(98);
-
+    exit(98);
     return (ptr);
 }
