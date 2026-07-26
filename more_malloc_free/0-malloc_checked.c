e0be68292a66df5846cc6a712f85453d0eaae252
Kev <kevin.hulbert93@gmail.com>
2026-07-26 11:14:32 +1000

    DOS to Unix was needed

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -8,8 +8,8 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
-    if (ptr == NULL)
-    exit(98);
-    return (ptr);
+void *ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
 }
