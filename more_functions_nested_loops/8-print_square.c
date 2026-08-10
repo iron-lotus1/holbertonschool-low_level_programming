efef19ab4c17a85a4507b6ceaedf9ee81a2a2db4
Kev <kevin.hulbert93@gmail.com>
2026-08-10 21:06:25 +1000

    updated code

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -5,20 +5,20 @@
 * @size: the width and Height
 * Return:
 */
-void print_square(int size)
+void print_square(int size) 
 {
-int row;
-int col;
-{
-_putchar('\n');
-return;
-}
-for (row = 0; row < size; row++)
-{
-for (col = 0; col < size; col++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
+    int row; 
+    int col;
+
+    if (size <= 0) {
+        _putchar('\n');
+        return;
+    }
+
+    for (row = 0; row < size; row++) {
+        for (col = 0; col < size; col++) {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
 }
