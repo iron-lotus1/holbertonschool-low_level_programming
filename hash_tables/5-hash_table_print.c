bc0ce86d22f9c219751c5f48f02ce3b595788891
Kev <kevin.hulbert93@gmail.com>
2026-08-18 12:35:15 +1000

    add 5-hash_table_print.c

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -28,7 +28,7 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				if (comma_flag == 1)
 					printf(", ");
-				
+
 				printf("'%s': '%s'", node->key, node->value);
 				comma_flag = 1;
 				node = node->next;
