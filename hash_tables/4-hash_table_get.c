9e65339b5c18fa6a0168c43466ba57d321d3b412
Kev <kevin.hulbert93@gmail.com>
2026-08-18 12:28:43 +1000

    add file

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -16,9 +16,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
-	
+
 	index = key_index((const unsigned char *)key, ht->size);
-	
+
 	node = ht->array[index];
 	while (node != NULL)
 	{
